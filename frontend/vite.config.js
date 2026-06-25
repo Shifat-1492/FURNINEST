c25e006bf3f848f4cdbfc11dca90ed246bd90af9
Istiak Ahmmed Shifat <178872959+Shifat-1492@users.noreply.github.com>
2026-06-25 17:08:08 +0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://furninest-bzdp.onrender.com',
         changeOrigin: true,
         secure: false,
       }
